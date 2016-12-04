c55740faf482504f640f0cf0e47e053238e6d8c2
Raphael Henrique Fontes da Silva <raphael.henrique@live.com>
2016-12-04 10:42:27 -0200

    Junta dois arquivos

--- src/Main.java
+++ src/Main.java
@@ -2,65 +2,75 @@ import java.io.*;
 
 public class Main {
 
+    static final String DELIMITADOR = "#$#";
+
     static final File arquivo_final = new File("DIRETORIO/");
     static final String diretorio ="DIRETORIO/";
     static final String file_name = "arquivo-0.txt";
+
+
 
     public static void main (String[] args){
         //String workingDir = System.getProperty("user.id");
 
         escreve_arquivo();
-        le_arquivo();
+
 
     }
 
 
-    public static void le_arquivo(){
+    public static StringBuffer le_arquivo(String nomeArquivo){
         int ch;
         StringBuffer strContent = new StringBuffer("");
+        File file = null;
+        FileInputStream fInput = null;
         try {
-            File f1 = new File(diretorio + "novo_arquivo.sar");
-            FileInputStream f1input = new FileInputStream(f1);
+            file = new File(diretorio + nomeArquivo);
+            fInput = new FileInputStream(file);
 
-            while ((ch=f1input.read()) !=-1){
+            while ((ch=fInput.read()) !=-1){
                 strContent.append((char)ch);
             }
 
-            f1input.close();
+            fInput.close();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        System.out.println(strContent);
+        //System.out.println(strContent);
 
+        return strContent;
     }
 
 
     public static void escreve_arquivo(){
-        int ch;
-        String strArquivo = "Novo arquivo\n" +
-                "arquivo gerado\n\n" +
-                "Novo";
 
+        int ch;
+
+        String strNovoArquivo = (   le_arquivo("arquivo-0.txt").toString() +
+                DELIMITADOR +       le_arquivo("ARQUIVO-4.txt").toString());
+
+        File file = null;
         FileOutputStream foutput = null;
 
         try {
-            File f1 = new File(diretorio + "novo_arquivo.sar");
+            file = new File(diretorio + "novo_arquivo.sar");
 
-            foutput = new FileOutputStream(f1);
+            foutput = new FileOutputStream(file);
 
-            if(!f1.exists()){
-                f1.createNewFile();
+            if(!file.exists()){
+                file.createNewFile();
             }
 
-            byte[] strBytes = strArquivo.getBytes();
+            byte[] strBytes = strNovoArquivo.getBytes();
 
             foutput.write(strBytes);
             foutput.flush();
             foutput.close();
 
             System.out.println("dpne");
+
         } catch (Exception e) {
             e.printStackTrace();
 
@@ -74,8 +84,14 @@ public class Main {
             }
         }
 
-        System.out.println(strArquivo);
+        System.out.println(strNovoArquivo);
 
     }
+
+
+    public static void descompactar(String arquivoSAR){
+        
+    }
+
 
 }
