ead61b012134cea665537843aa19c9219e349554
egrojCarvalho <egrojcarvalho@hotmail.com>
2017-01-31 16:02:12 -0300

    Busca esta funcionando
    Criei 5 filhos para testar as buscas

--- trabalho2/Arvore-B/main.c
+++ trabalho2/Arvore-B/main.c
@@ -1,163 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 100
+#define MAX 1000
 
-/*
-*****************TRABALHO 2******************
-ORI (ORGANIZACAO E RECUPERACAO DA INFORMACAO)
------------------ARVORE-B--------------------
-
-INTEGRANTES DO GRUPO:
-
-JORGE
-LUCAS
-RAPHAEL
-
-*/
 typedef struct No No;
 int grau_arvore;
 int mediana;
 
-/*Cada nó, possui:
-chave[max-1]: as chaves, em ordem crescente
-num_chaves: numero de chaves do nó
-folha: 1 - se é nó folha, 0 - se é nó interno
-raiz: se é nó raiz ou não
-ponteiro: ponteiros para os filhos do nó
-*/
 struct No
 {
-    int chave[MAX - 1];
+    int chave[MAX-1];
     int id;
     int num_chaves, folha, raiz;
     No *ponteiro[MAX];
 };
 
-int Verifica_num_chaves(No atual);
-void Divide_No(No *atual);
-void Inserindo_chave(No *atual, int key);
-int Busca_chave(No *atual, int key);
-void Criar_arvore(No *atual);
-void Mostra_arvore(No *raiz);
-
-int Verifica_num_chaves(No atual) {
-    if (atual.raiz == 1 || atual.num_chaves >= (grau_arvore - 1) && atual.num_chaves <= (2 * grau_arvore - 1))
+int Verifica_num_chaves(No atual){
+    if(atual.raiz == 1 || atual.num_chaves >= (grau_arvore - 1) && atual.num_chaves <= (2 * grau_arvore - 1)  )
         return 1;
-    else if (atual.num_chaves >= grau_arvore)
+    else if(atual.num_chaves >= grau_arvore)
         Divide_No(&atual);
-    return 0;
+        return 0;
 }
 
-/*
-Se o numero de chaves for maior que maximo: 2t-1 chaves,
-vai dividir o no
-*/
-void Divide_No(No *atual) {
-    
-    
+void Divide_No(No *atual){
+
+
 
 }
 
-void Mostra_arvore(No *raiz) {
-    
-    for (int i = 0; i <= (raiz->num_chaves); i++) {
-        int num_chaves_filho;
-        if (raiz->ponteiro[i] != NULL) {
-            num_chaves_filho = raiz->ponteiro[i]->num_chaves;
-        
-            for (int j = 0; j <= num_chaves_filho; j++) {
-                printf("valor i %d", i);
-                printf("valor j %d", j);
-            }
-        }
-    }
-
-}
-
-void Inserindo_chave(No *atual, int key) {
+void Inserindo_chave(No *atual){
     int aux = Verifica_num_chaves(*atual);
-
-    if (aux == 1) {
+    int key;
+    printf("Insira a chave: ");
+    scanf("%d", &key);
+    if(aux == 1){
         atual->chave[atual->num_chaves] = key;
         atual->num_chaves++;
         printf("Num chaves: %d", atual->num_chaves);
     }
-    else{
-        
-    }
 }
 
-int Busca_chave(No *atual, int key) {
-    int i = -1;
+int Busca_chave(No atual, int key){
+    int i = 0;
 
-    do {
-        if (key == atual->chave[i]) {
-            printf("Foi encontrada no indice: %d", i);
-            return key;
-        }
-        if (atual->folha == 0) {
-            if (key < atual->chave[i] && atual->folha == 0)
-                return Busca_chave(&atual->ponteiro[i], key);
-            else if (i == atual->num_chaves && key > atual->chave[i])
-                return Busca_chave(&atual->ponteiro[i + 1], key);
+    do{
+            if(key == atual.chave[i]){
+                printf("Foi encontrada no indice: %d", i);
+                return key;
+            }
+            else if(key < atual.chave[i] && atual.folha == 0)
+                return Busca_chave(*atual.ponteiro[i], key);
+
+            else if(i == atual.num_chaves && key > atual.chave[i])
+                return Busca_chave(*atual.ponteiro[i+1], key);
             i++;
-        }
-    } while (i == atual->num_chaves + 1);
+
+    } while (i < atual.num_chaves + 1);
     printf("Busca sem sucesso");
     return 0;
 }
 
-void Criar_arvore(No *atual) {
-
-    atual->ponteiro[0] = NULL;
+void Criar_arvore(No *atual){
 
     atual->num_chaves = 0;
     atual->raiz = 1;
-    atual->folha = 1;
+    atual->folha = 0;
 
 }
 
 
 int main()
 {
-    No *T;
-    T = malloc(MAX * sizeof(No));
+    No T;
+    No filho1, filho2, filho3, filho4, filho5;
+
     Criar_arvore(&T);
     int key;
-    int opcao;
+    T.chave[0] = 10;
+    T.chave[1] = 15;
+    T.chave[2] = 20;
+    T.chave[3] = 30;
+    T.ponteiro[0] = &filho1;
+    T.ponteiro[1] = &filho2;
+    T.ponteiro[2] = &filho3;
+    T.ponteiro[3] = &filho4;
+    T.ponteiro[4] = &filho5;
+    filho1.chave[0] = 5;
+    filho1.chave[1] = 6;
+    filho1.chave[2] = 7;
+    filho1.chave[3] = 8;
+    filho2.chave[0] = 11;
+    filho2.chave[1] = 12;
+    filho2.chave[2] = 13;
+    filho2.chave[3] = 14;
+    filho3.chave[0] = 16;
+    filho3.chave[1] = 17;
+    filho3.chave[2] = 18;
+    filho3.chave[3] = 19;
+    filho4.chave[0] = 21;
+    filho4.chave[1] = 22;
+    filho4.chave[2] = 23;
+    filho4.chave[3] = 24;
+    filho5.chave[0] = 31;
+    filho5.chave[1] = 32;
+    filho5.chave[2] = 33;
+    filho5.chave[3] = 40;
+
+    filho1.num_chaves = 4;
+    filho2.num_chaves = 4;
+    filho3.num_chaves = 4;
+    filho4.num_chaves = 4;
+    filho5.num_chaves = 4;
 
     printf("Digite o grau da Arvore B: ");
     scanf("%d", &grau_arvore);
 
-    mediana = grau_arvore / 2;
-    printf("%d", mediana);
-
-    
-    do {
-        printf("\n-------ESCOLHA SUA OPCAO-------:\n\n\t 1.Insercao\n\t 2.Busca\n\t 3.Sair\n4.Mostra arvore\n");
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-
-        case 1:
-            printf("Informe a chave: ");
-            scanf("%d", &key);
-            Inserindo_chave(&T, key);
-            break;
-        case 2:
-            printf("Informe a chave: ");
-            scanf("%d", &key);
-            Busca_chave(&T, key);
-
-            break;
-        case 3:
-            printf("Saindo...");
-            break;
-        case 4:
-            Mostra_arvore(&T);
-            break;
-        }
-    } while (opcao != 3);
+    printf("Informe a chave: ");
+    scanf("%d", &key);
+    T.num_chaves = 3;
+    int aux = Busca_chave(T, key);
 
     return 0;
 }
